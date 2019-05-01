a1f36bf0870938b2fe08a49a1f29e9e7a12b265e
Maisy Chung <maisyleigh@gmail.com>
2019-05-01 16:41:57 -0400

    Initial Commit

---
+++ .cache/sync-requires.js
@@ -0,0 +1,32 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/dev-404-page.js")),
+  "component---src-templates-blog-template-js": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/src/templates/blog-template.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/src/pages/page-2.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "hello-world.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/hello-world.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "another-one.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/another-one.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "404.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/404.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/index.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "page-2.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/page-2.json"),
+  "layout-index.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "layout---index": preferDefault(require("/Users/MC/Desktop/MaisyBlogFixed/codestack-gatsby-blog/.cache/layouts/index.js"))
+}
